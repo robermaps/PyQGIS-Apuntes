9a923d109ba506471753ed4279f775fafaf515da
Rober J <programapa.contacta@gmail.com>
2021-11-03 20:44:26 +0100

    Update Field_calculator_expressions.sql

--- Field_calculator_expressions.sql
+++ Field_calculator_expressions.sql
@@ -1,5 +1,5 @@
-# Number of vertices in a polygon:
+-- Number of vertices in a polygon:
 num_points($geometry)
 
-# Calculate distance from centroid of polygon to its nearest vertice
+-- Calculate distance from centroid of polygon to its nearest vertice
 distance(centroid($geometry),start_point($geometry))
